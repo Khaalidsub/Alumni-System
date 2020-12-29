a7dae7452516b4641644d04291bc5af08273f5c5
Muaz Amir Nur Rashid <muazamir99@gmail.com>
2020-12-29 10:03:43 +0800

    ok

--- Login/db_conn.php
+++ Login/db_conn.php
@@ -8,6 +8,9 @@ $db_name = "alumni_account";
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
+$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $uname, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 if (!$conn) {
 	echo "Connection failed!";
 }
--- Login/signup-check.php
+++ Login/signup-check.php
@@ -13,50 +13,53 @@ if (isset($_POST['uname']) && isset($_POST['password'])
 	}
 
 	$uname = validate($_POST['uname']);
-	$pass = validate($_POST['password']);
+	$pass = $_POST['password'];
 
-	$re_pass = validate($_POST['confirm_password']);
+	$re_pass = $_POST['confirm_password'];
 	$email = validate($_POST['email']);
 
+	$id = 2;
 	$user_data = 'uname'. $uname. 'email'. $email;
 
 
 	if (empty($uname)) {
-		header("Location: index.php?error=username is required");
+		header("Location: index.php?error=username is required&$user_data");
 	    exit();
 	}else if(empty($pass)){
-        header("Location: index.php?error=Password is required");
+        header("Location: index.php?error=Password is required&$user_data");
 	    exit();
 	}
 	else if(empty($re_pass)){
-        header("Location: index.php?error=Confirmation password is required");
+        header("Location: index.php?error=Confirmation password is required&$user_data");
 	    exit();
 	}
 
 	else if(empty($email)){
-        header("Location: index.php?error=Email is required");
+        header("Location: index.php?error=Email is required&$user_data");
 	    exit();
 	}
 
 	else if($pass !== $re_pass){
-        header("Location: index.php?error=confirmation password does not match");
+        header("Location: index.php?error=confirmation password does not match&$user_data");
 	    exit();
 	}
 
 	else{
-
-		// hash password
-        $pass = md5($pass);
 
 	    $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass' AND email='$email'"	;
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
 			header("Location: index.php?error=The username is taken try another&$user_data");
-	        exit();
+			exit();
+			 
 		}else {
-           $sql2 = "INSERT INTO users(user_name, password, email) VALUES('$uname', '$pass', '$email')";
-           $result2 = mysqli_query($conn, $sql2);
+
+			$sql2 = "INSERT INTO users (id,user_name, password, email ) VALUES(?,?,?,?)";
+			$stmtinsert = $db->prepare($sql2);
+			$result2 = $stmtinsert->execute([$id, $uname, $pass, $email]);
+
+
            if ($result2) {
            	 header("Location: index.php?success=Your account has been created successfully");
 	         exit();
@@ -65,6 +68,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])
 		        exit();
            }
 		}
+		
 	}
 	
 }else{
