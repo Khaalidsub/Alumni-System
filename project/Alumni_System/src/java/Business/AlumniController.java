a4ab28b7e019edefdcb148484feb16e6f617822b
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-30 21:52:20 +0800

    alumni controller for alumni management added

--- project/Alumni_System/src/java/Business/AlumniController.java
+++ project/Alumni_System/src/java/Business/AlumniController.java
@@ -5,6 +5,8 @@
  */
 package Business;
 
+import Middleware.Alumni;
+import Middleware.AlumniAddress;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -13,6 +15,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Middleware.Alumni;
+import Middleware.AlumniAddress;
+import Middleware.EduLevel;
+import Middleware.alumniTitle;
+
 /**
  *
  * @author SleepingLotus
@@ -20,69 +27,92 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "AlumniController", urlPatterns = {"/AlumniController"})
 public class AlumniController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AlumniController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AlumniController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+private Alumni m_Alumni;
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	public AlumniController(){
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	}
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+	public void finalize() throws Throwable {
+
+	}
+	public Alumni getAlumni(){
+		return m_Alumni;
+	}
+
+	/**
+	 * 
+	 * @param alumniEmail
+	 */
+	public Alumni getAlumniInfo(String alumniEmail){
+		return null;
+	}
+
+	public Alumni[] getAlumniList(){
+		return null;
+	}
+
+	
+	/**
+	 * 
+	 * @param alumniEmail
+	 */
+	public Alumni getDetailedAlumniInfo(String alumniEmail){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param alumniName
+	 * @param query
+	 */
+	public Alumni[] getFilteredAlumniList(String alumniName, String query){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param alumniName
+	 */
+	public Alumni[] getSearchedAlumni(String alumniName){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param newVal
+	 */
+	public void setAlumni(Alumni newVal){
+		m_Alumni = newVal;
+	}
+
+
+
+	/**
+	 * 
+	 * @param address
+	 * @param alumniEmail
+	 * @param phoneNo
+	 * @param eduLevel
+	 * @param graduateYear
+	 * @param courseName
+	 */
+	public void updateAlumniInfo(AlumniAddress address, String alumniEmail, int phoneNo, EduLevel eduLevel, int graduateYear, String courseName){
+
+	}
+
+	/**
+	 * 
+	 * @param address
+	 * @param eduLevel
+	 * @param phoneNumber
+	 * @param title
+	 * @param courseName
+	 * @param graudateYear
+	 */
+	public boolean validateFields(AlumniAddress address, EduLevel eduLevel, int phoneNumber, alumniTitle title, String courseName, int graudateYear){
+		return false;
+	}
+
 
 }
