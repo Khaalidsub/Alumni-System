8cad1ef3e5b08be05ab22eb2b3c3456b3d28b272
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-24 19:01:18 +0800

    alumni management module integration ..1

--- integratedallsysmtem/src/java/Business/AlumniController.java
+++ integratedallsysmtem/src/java/Business/AlumniController.java
@@ -1,109 +1,383 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Business;
 
+import jdbc.AlumniDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import Middleware.Alumni;
+import Middleware.AlumniAddress;
+import Middleware.EduLevel;
+import Middleware.alumniTitle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 
 /**
- * @author Asus
- * @version 1.0
- * @created 30-Dec-2020 10:06:36 PM
+ *
+ * @author SleepingLotus
  */
-public class AlumniController {
+@WebServlet(name = "AlumniController", urlPatterns = {"/AlumniController"})
+public class AlumniController extends HttpServlet {
 
-	//private Database m_Database;
+    private AlumniDAO alumniDao;
 
-	public AlumniController(){
+    @Override
+    public void init() throws ServletException {
+        alumniDao = AlumniDAO.getInstance();
 
-	}
+    }
 
-	public void finalize() throws Throwable {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-	}
+        String command = request.getParameter("command");
 
-	public boolean checkBothNew(){
-		return false;
-	}
+        try {
 
-	public void checkPicFormat(){
+            // if the command is missing, then default to login
+            if (command == null) {
+                command = "ALUMNI-INFO";
+            }
 
-	}
+            switch (command) {
+                case "ALUMNI-SEARCH":
+                    getAlumniList(request, response);
+                    break;
 
-	public void editProfile(){
+                case "MY-PROFILE":
+                    getAlumniInfo(request, response);
+                    break;
+                case "ALUMNI-INFO":
+                    getAlumniInfo(request, response);
 
-	}
+                case "INFO-DETAIL":
+                    getDetailedAlumniInfo(request, response);
 
-	/**
-	 * 
-	 * @param Name
-	 * @param password
-	 * @param Address
-	 * @param CurrentJob
-	 * @param PreviousJob
-	 * @param Email
-	 * @param GraduateYear
-	 * @param SalaryCurrent
-	 * @param SalaryPrevious
-	 * @param Status
-	 * @param Phone
-	 */
-	public String enterDetails(String Name, String password, String Address, String CurrentJob, String PreviousJob, String Email, int GraduateYear, double SalaryCurrent, double SalaryPrevious, String Status, String Phone){
-		return "";
-	}
+                case "EDIT-PROFILE":
+                    updateAlumniInfoPage(request, response);
 
-	/*public Database getDatabase(){
-		return m_Database;
-	}*/
+                    break;
 
-	/**
-	 * 
-	 * @param newPass
-	 * @param reenterPass
-	 */
-	public String newPassReenter(String newPass, String reenterPass){
-		return "";
-	}
+            }
 
-	/**
-	 * 
-	 * @param newPass
-	 */
-	public String newPassword(String newPass){
-		return "";
-	}
+        } catch (Exception exc) {
+            throw new ServletException(exc);
+        }
 
-	/**
-	 * 
-	 * @param password
-	 */
-	public String oldPass(String password){
-		return "";
-	}
+    }
 
-	public void reqUserInfo(){
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String command = request.getParameter("command");
 
-	}
+        try {
 
-	public void retUserDetails(){
+            // if the command is missing, then default to login
+            if (command == null) {
+                command = "ALUMNI-INFO";
+            }
 
-	}
+            switch (command) {
+                case "UPDATE-ALUMNI":
+                    updateAlumniInfo(request, response);
 
-	/**
-	 * 
-	 * @param newVal
-	 */
-	/*public void setDatabase(Database newVal){
-		m_Database = newVal;
-	}*/
+                case "SEARCH-ALUMNI":
+                    getSearchedAlumni(request, response);
+                    break;
+                case "FILTER-ALUMNI":
+                    getFilteredAlumniList(request, response);
+                    break;
+            }
 
-	public void updatePass(){
+        } catch (Exception exc) {
+            throw new ServletException(exc);
+        }
+    }
 
-	}
+    public void finalize() throws Throwable {
 
-	public void uploadPic(){
+    }
 
-	}
+    public void updateAlumniInfo(HttpServletRequest request, HttpServletResponse response) {
 
-	public String uploadPicture(){
-		return "";
-	}
+        String streetName = request.getParameter("streetName");
+        String houseNo = request.getParameter("houseNo");
+        String postalCode = request.getParameter("postalCode");
+        String state = request.getParameter("state");
+        String city = request.getParameter("city");
+        String country = request.getParameter("country");
+        String region = request.getParameter("region");
+        String title = request.getParameter("title");
+        String graduateYear = request.getParameter("graduateYear");
+        String phone = request.getParameter("phone");
 
+        try {
+            Alumni alumni = alumniDao.getDetailedAlumniInfo(request.getParameter("email"));
+
+            AlumniAddress alumniAddress = alumniDao.getAlumniAddressInfo(alumni.getAlumniAddressID());
+            alumni.setPhoneNo(Integer.valueOf(phone));
+            alumni.setTitle(alumniTitle.valueOf(title));
+            alumni.setGraduateYear(Integer.valueOf(graduateYear));
+            alumni.setAlumniEmail(request.getParameter("email"));
+
+            alumniAddress.setCity(city);
+            alumniAddress.setCountry(country);
+            alumniAddress.setHouseNo(houseNo);
+            alumniAddress.setRegion(region);
+            alumniAddress.setState(state);
+            alumniAddress.setPostalCode(postalCode);
+            alumniAddress.setStreetName(streetName);
+
+            alumni.setAlumniAddress(alumniAddress);
+            System.out.println("Business.AlumniController.updateAlumniInfo() : " + alumniAddress);
+
+            alumniDao.updateAlumniDetails(alumni);
+            request.setAttribute("alumniEmail", alumni.getAlumniEmail());
+            getAlumniInfo(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     *
+     * @param alumniEmail
+     */
+    public void getAlumniInfo(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            Alumni alumni;
+            System.out.println(request.getParameter("alumniEmail"));
+            if (request.getParameter("alumniEmail") != null) {
+                alumni = alumniDao.getAlumniInfo(request.getParameter("alumniEmail"));
+            } else {
+                alumni = alumniDao.getAlumniInfo("6naseer.far@wditu.com");
+            }
+            RequestDispatcher dispatcher;
+            dispatcher = request.getRequestDispatcher("/alumni/alumniProfile.jsp");
+            request.setAttribute("alumni", alumni);
+            dispatcher.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(AlumniController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void getAlumniList(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            List<Alumni> alumnis = alumniDao.getAlumniList();
+            RequestDispatcher dispatcher;
+            dispatcher = request.getRequestDispatcher("/alumni/search_alumni.jsp");
+            System.out.println(alumnis.get(0).toString());
+            request.setAttribute("ALUMNI_LIST", alumnis);
+            dispatcher.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(AlumniController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updateAlumniInfoPage(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            Alumni alumni;
+            AlumniAddress alumniAddress;
+            System.out.println(request.getParameter("alumniEmail"));
+            if (request.getParameter("alumniEmail") != null) {
+                alumni = alumniDao.getDetailedAlumniInfo(request.getParameter("alumniEmail"));
+                alumniAddress = alumniDao.getAlumniAddressInfo(alumni.getAlumniAddressID());
+            } else {
+                alumni = alumniDao.getDetailedAlumniInfo("6naseer.far@wditu.com");
+                alumniAddress = alumniDao.getAlumniAddressInfo(alumni.getAlumniAddressID());
+            }
+            RequestDispatcher dispatcher;
+            dispatcher = request.getRequestDispatcher("/alumni/edit_information.jsp");
+            request.setAttribute("alumni", alumni);
+            request.setAttribute("alumniAddress", alumniAddress);
+            dispatcher.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(AlumniController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     *
+     * @param alumniEmail
+     */
+    public void getDetailedAlumniInfo(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Alumni alumni;
+            AlumniAddress alumniAddress;
+            System.out.println(request.getParameter("alumniEmail"));
+            if (request.getParameter("alumniEmail") != null) {
+                alumni = alumniDao.getDetailedAlumniInfo(request.getParameter("alumniEmail"));
+                alumniAddress = alumniDao.getAlumniAddressInfo(alumni.getAlumniAddressID());
+            } else {
+                alumni = alumniDao.getDetailedAlumniInfo("6naseer.far@wditu.com");
+                alumniAddress = alumniDao.getAlumniAddressInfo(alumni.getAlumniAddressID());
+            }
+            RequestDispatcher dispatcher;
+            dispatcher = request.getRequestDispatcher("/alumni/alumniInfo.jsp");
+            request.setAttribute("alumni", alumni);
+            request.setAttribute("alumniAddress", alumniAddress);
+            dispatcher.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(AlumniController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     *
+     * @param alumniName
+     * @param query
+     */
+    public void getFilteredAlumniList(HttpServletRequest request, HttpServletResponse response) {
+        List<Alumni> alumnis = new ArrayList<>();
+        try {
+            String alumniName = request.getParameter("alumniName");
+            String query = request.getParameter("filter");
+            String queryValue = request.getParameter(query+"_input");
+      
+            alumnis = alumniDao.getFilteredAlumni(alumniName,query,queryValue);
+            RequestDispatcher dispatcher;
+            dispatcher = request.getRequestDispatcher("/alumni/search_alumni.jsp");
+            request.setAttribute("ALUMNI_LIST", alumnis);
+            dispatcher.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(AlumniController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     *
+     * @param alumniName
+     */
+    public void getSearchedAlumni(HttpServletRequest request, HttpServletResponse response) {
+        List<Alumni> alumnis = new ArrayList<>();
+
+        try {
+            alumnis = alumniDao.getSearchedAlumni(request.getParameter("alumniName"));
+            RequestDispatcher dispatcher;
+            dispatcher = request.getRequestDispatcher("/alumni/search_alumni.jsp");
+            request.setAttribute("ALUMNI_LIST", alumnis);
+            dispatcher.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(AlumniController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     *
+     * @param address
+     * @param alumniEmail
+     * @param phoneNo
+     * @param eduLevel
+     * @param graduateYear
+     * @param courseName
+     */
+    /*  public void updateAlumniInfo(HttpServletRequest request, HttpServletResponse response) throw Exception{
+        
+        Alumni alumni = new Alumni();
+        
+        
+    } */
+    /**
+     *
+     * @param address
+     * @param eduLevel
+     * @param phoneNumber
+     * @param title
+     * @param courseName
+     * @param graudateYear
+     */
+    public boolean validateFields(AlumniAddress address, EduLevel eduLevel, int phoneNumber, alumniTitle title, String courseName, int graudateYear) {
+        return false;
+    }
+    
+     public void validateUser(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+         
+//         String command = request.getParameter("command");
+//        String userType = request.getSession("userType");
+////       int register = request.getSession("login");
+//         
+//         if (userType == "UTM_Alumni"){
+//             
+//             if (login == 1){
+//               
+//                if (register == 1){
+//                try {
+//
+//            
+//            if (command == null) {
+//                command = "ALUMNI-INFO";
+//            }
+//
+//            switch (command) {
+//                case "UPDATE-ALUMNI":
+//                    updateAlumniInfo(request, response);
+//
+//                case "SEARCH-ALUMNI":
+//                    getSearchedAlumni(request, response);
+//                    break;
+//                case "FILTER-ALUMNI":
+//                    getFilteredAlumniList(request, response);
+//                    
+//                case "INFO-DETAIL":
+//                    getDetailedAlumniInfo(request, response);
+//                    
+//                case "ALUMNI-INFO":
+//                    getAlumniInfo(request, response);
+//                    
+//                    break;
+//            }
+//
+//        } catch (Exception exc) {
+//            throw new ServletException(exc);
+//        }
+//        }
+//                 else {
+//                    try {
+//            if (command == null) {
+//                command = "ALUMNI-INFO";
+//            }
+//
+//            switch (command) {
+//                
+//
+//                case "SEARCH-ALUMNI":
+//                    getSearchedAlumni(request, response);
+//                    break;
+//                case "FILTER-ALUMNI":
+//                    getFilteredAlumniList(request, response);
+//                    
+//                case "INFO-DETAIL":
+//                    getDetailedAlumniInfo(request, response);
+//                    
+//                case "ALUMNI-INFO":
+//                    getAlumniInfo(request, response);
+//                    
+//                    break;
+//            }
+//
+//        } catch (Exception exc) {
+//            throw new ServletException(exc);
+//        }
+//            }   
+//            } else {   getDetailedAlumniInfo(request, response);}  
+//             
+//        } else { getDetailedAlumniInfo(request, response);}     
+    }
 }
