b66e997a8407ad62d150a979fa5a68098eb3e93c
ryan-dono <adoryanjunior28@gmail.com>
2021-01-25 17:46:45 +0800

    Added API for getting alumni list

---
+++ integratedallsysmtem/src/java/webservice/mobileAPI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservice;
+import jdbc.AlumniDAO;
+import Middleware.Alumni;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author hp
+ */
+@Path("alumni")
+public class mobileAPI {
+    @GET
+    @Path("/getdata") // this path is used to identify method
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Alumni> getDatainJSON(){
+         AlumniDAO a = AlumniDAO.getInstance();
+        return a.getAlumniList();
+       
+    }
+}
