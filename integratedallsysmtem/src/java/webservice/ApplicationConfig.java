5d3f4351efdb30423e1d8429a46e821ad139da0c
Afiq <afiqtechno96@gmail.com>
2021-01-27 09:05:14 -0800

    .

--- integratedallsysmtem/src/java/webservice/ApplicationConfig.java
+++ integratedallsysmtem/src/java/webservice/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(webservice.ReportingResource.class);
         resources.add(webservice.mobileAPI.class);
         resources.add(webservice.userData.class);
     }
